ca8767cf0817fcf023f22578c55a42bb030fc576
OsmarVitor <perez_vitor@hotmail.com>
2023-01-06 09:43:47 -0300

    create controller

---
+++ src/main/java/com/example/newrelic/controller/ControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.newrelic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerTest {
+
+    @GetMapping
+    public String teste(){
+        return "Hello world";
+    }
+}
